4ec1ccff93f0d23e1fe943f7def3b39f08217e5f
Maksym Nenashev <57881489+maks-nenashev@users.noreply.github.com>
2022-12-24 19:28:58 +0100

    new migration

--- db/migrate/20221221222638_add_products.rb
+++ db/migrate/20221221222638_add_products.rb
@@ -10,7 +10,7 @@ class AddProducts < ActiveRecord::Migration[7.0]
                  :is_spicy => false, 
                  :is_veg => false, 
                  :is_best_offer => false, 
-                 :path_to_image => '/images/1.jpeg'
+                 :path_to_image => '/images/1.jpg'
 
   Product.create :title => 'Pepperoni',                                                                      
                  :description => 'Nice Pepperoni pizza!', 
@@ -19,7 +19,7 @@ class AddProducts < ActiveRecord::Migration[7.0]
                  :is_spicy => false, 
                  :is_veg => false, 
                  :is_best_offer => true, 
-                 :path_to_image => '/images/2.jpeg'
+                 :path_to_image => '/images/2.jpg'
 
   Product.create :title => 'Vegetarian', 
                  :description => 'Amazing Vegetarian pizza!', 
@@ -28,7 +28,7 @@ class AddProducts < ActiveRecord::Migration[7.0]
                  :is_spicy => false, 
                  :is_veg => false, 
                  :is_best_offer => false, 
-                 :path_to_image => '/images/3.jpeg'
+                 :path_to_image => '/images/3.jpg'
 
     end
  end
