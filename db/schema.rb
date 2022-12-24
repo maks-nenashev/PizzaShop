4fb3107e1492b20abb9f5caca3c53924062d5aa8
Maksym Nenashev <57881489+maks-nenashev@users.noreply.github.com>
2022-12-24 17:38:03 +0100

    schema

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema[7.0].define(version: 2022_12_21_222638) do
   create_table "products", force: :cascade do |t|
-    t.string "titel"
+    t.string "title"
     t.text "description"
     t.decimal "prise"
     t.decimal "size"
