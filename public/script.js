97c8ce2e7abaf7aa03cc9b2a39b9f0641ee0a840
Maksym Nenashev <57881489+maks-nenashev@users.noreply.github.com>
2022-12-27 18:13:46 +0100

    kol--wo itomow

--- public/script.js
+++ public/script.js
@@ -19,7 +19,8 @@ function add_to_cart(id)
     x = x * 1 + 1;
     
     window.localStorage.setItem(key,x);
-    
+      // wywod kol-wo itomow  w window s podsczetom
+    alert('Items in your cart:' + cart_get_number_of_items()); 
 }
 
 function cart_get_number_of_items()
