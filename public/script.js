84cbce490d963af139daa2d62935751747d0f6f2
Maksym Nenashev <57881489+maks-nenashev@users.noreply.github.com>
2022-12-27 17:52:11 +0100

    korzina

--- public/script.js
+++ public/script.js
@@ -22,3 +22,20 @@ function add_to_cart(id)
     
 }
 
+function cart_get_number_of_items()
+{
+  var cnt = 0;
+
+  for (var i = 0; i < window.localStorage.length; i++)
+  {
+    var key = window.localStorage.key(i); // получаем ключ
+    var value = window.localStorage.getItem(key); // получаем значение
+
+    if(key.indexOf('product_') == 0)
+    {
+      cnt = cnt + value * 1;
+    }
+  }
+
+  return cnt;
+}
