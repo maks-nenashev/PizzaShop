efb235d99c7cbbc7aafab6dc2cafb53c8bda2487
Maksym Nenashev <57881489+maks-nenashev@users.noreply.github.com>
2023-01-11 18:32:02 +0100

    shema new

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_21_222638) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_11_151707) do
+  create_table "orders", force: :cascade do |t|
+    t.text "orders_input"
+    t.text "name"
+    t.text "phone"
+    t.text "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "products", force: :cascade do |t|
     t.string "title"
     t.text "description"
